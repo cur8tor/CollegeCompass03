9b6e4d0f06746ac84f9e3c2efddfd1c0c848fd74
The Curator <tmartzdc@gmail.com>
2024-01-21 14:05:08 -0600

    Ieye

--- CollegeCompassV03/src/components/NavBar.js
+++ CollegeCompassV03/src/components/NavBar.js
@@ -63,7 +63,7 @@ const NavBar = () => {
                     exact
                     activeClassName="router-link-exact-active"
                   >
-                    External API
+                    External APIIII
                   </NavLink>
                 </NavItem>
               )}
